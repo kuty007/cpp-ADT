fe56bcaed0fc340a572d415c07a020877da1ce29
kuty007 <asaf701@gmail.com>
2023-03-13 19:09:54 +0200

    first

--- AdptArray.c
+++ AdptArray.c
@@ -24,34 +24,47 @@ PAdptArray CreateAdptArray(COPY_FUNC copyElement, DEL_FUNC freeElement, PRINT_FU
     array->copyElement = copyElement;//initialize the copy function
     array->freeElement = freeElement;//initialize the delete function
     array->printElement = printElement;//initialize the print function
-    array->array = NULL;//initialize array to NULL
+    array->array = NULL;//initialize the array to NULL
+
     return array;
 }
 
 Result SetAdptArrayAt(PAdptArray array, int index, PElement element) {
+    if (array == NULL) {//if the array is null
+        return FAIL;
+    }
     if (index >= array->size) {//if the index is bigger than the size of the array
-        array->array = (void **) realloc(array->array, sizeof(void *) * (index + 1));//allocate space for the new index
-        if (array->array == NULL) {
-            printf("Error: realloc has failed\n");
-            exit(1);
+        void **newArray = (void **) calloc(index + 1, sizeof(void *));//allocate space for a new array
+        if (newArray == NULL) {
+            printf("Error: malloc has failed\n");
+            return FAIL;
         }
-        for (int i = array->size; i < index; i++) {//initialize the new indexes to NULL
-            array->array[i] = NULL;//NULL
+        for (int i = 0; i < array->size; i++) {//for each index in the array
+            newArray[i] = array->array[i];//copy the old array to the new array
         }
-        array->size = index + 1;//update the size of the array
+        free(array->array);//free the old array
+        array->array = newArray;//set the array to the new array
+        array->size = index + 1;//set the size to the index + 1
     }
     if (array->array[index] != NULL) {//if the index is not null
-        array->freeElement(array->array[index]);//delete_person or delete_book
+        array->freeElement(array->array[index]);//delete the element at the index
     }
-    array->array[index] = array->copyElement(element);//copy_person or copy_book
+    array->array[index] = array->copyElement(element);//copy the element to the index
+    return SUCCESS;
 }
+
+//get a copy of the element at the index
 
 PElement GetAdptArrayAt(PAdptArray array, int index) {
     if (index >= array->size) {//if the index is bigger than the size of the array
-        return NULL;//NULL
+        return NULL;
     }
-    return array->array[index];//return the element in the index
+    if (array->array[index] == NULL) {//if the index is null
+        return NULL;
+    }
+    return array->copyElement(array->array[index]);//copy_person or copy_book
 }
+
 
 int GetAdptArraySize(PAdptArray array) {
     return array->size;//return the size of the array
@@ -69,7 +82,7 @@ void DeleteAdptArray(PAdptArray array) {
     if (array->array != NULL) {
         for (int i = 0; i < array->size; i++) {//for each index in the array
             if (array->array[i] != NULL) {//if the index is not null
-                array->freeElement(array->array[i]);//delete_person or delete_book
+                array->freeElement((array->array)[i]);//delete_person or delete_book
             }
         }
         free(array->array);//free the array
